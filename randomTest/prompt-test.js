f1dbfbc5ae271a4a24b39b51d5fccedaf953156c
Martin Banks <anotherbanksy@mac.com>
2016-05-28 16:52:17 +1000

    recommit to new repo

---
+++ randomTest/prompt-test.js
@@ -0,0 +1,26 @@
+// using npm prompt commaand
+// does it work in web browser?
+
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['number'], function(err, result){
+	if (result.number > 10){
+		console.log("over 10!!")
+	} else {
+		console.log("under 10 :( ")
+	}
+
+	//console.log(result.number);
+});
+
+
+
+
+
+
+
+
+
+
